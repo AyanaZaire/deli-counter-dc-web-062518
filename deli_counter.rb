ca3ce2ba7948cc24b46c876d87050e21c19e01a6
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 18:08:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
-def line(deli)
-  if deli < 0 
-    puts "The line is currently empty."
-  
-end
-
+else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
 
